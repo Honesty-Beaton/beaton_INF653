13201680b10e472dfc84ed2942ea6dc0ff63698e
Honesty Beaton <h_beaton@mail.fhsu.edu>
2025-02-06 18:39:58 -0600

    Completed Challenge 1

---
+++ Assignment 1/assignment1.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Challenge 1*/
+$name = "John";
+$age = 25;
+$favoriteColor = "blue";
+
+echo "My name is " . $name . ", I am " . $age . " years old and my favorite color is " . $favoriteColor .".";
+
+?>
